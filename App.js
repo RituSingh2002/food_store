17ab943becb992046953e92d4352e22c9600b48e
RituSingh2002 <ritusingh29g@gmail.com>
2021-06-26 12:47:55 +0530

    dd

---
+++ App.js
@@ -0,0 +1,70 @@
+var map = L.map('map').setView([20.593683,78.962883], 5);
+const tileUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+//attribution means copyright in map
+const attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Coded by coder\'s gyan with ❤️';
+const tileLayer = L.tileLayer(tileUrl, { attribution });
+tileLayer.addTo(map);
+function generateList(){
+    const ul=document.querySelector('.list');
+   storeList.forEach((shop)=>{
+const li=document.createElement('li');
+const div=document.createElement('div');
+const a=document.createElement('a');
+const p=document.createElement('p');
+a.addEventListener('click',()=>{
+flyToStore(shop);
+});
+div.classList.add('shop-item');
+a.innerText=shop.properties.name;
+a.href="#";
+p.innerText=shop.properties.address;
+div.appendChild(a);
+div.appendChild(p);
+li.appendChild(div);
+ul.appendChild(li);
+
+   })
+
+}
+generateList();
+function popupContent(shop){
+    return `
+    <div>
+    <h4>${shop.properties.name}</h4>
+    <p>${shop.properties.address}</p>
+    </div>
+    <div class="Phone-number">
+  <a href="tel:${shop.properties.phone}">${shop.properties.phone}</a>
+    </div>
+    `
+
+     
+}
+function onEachFeature(feature,layer){
+    layer.bindPopup(popupContent(feature),{closeButton:false,offset:L.point(0,-8)});
+
+}
+var myicon=L.icon({
+     iconUrl:"ss.png",
+        iconSize:[30,40]
+    });
+const shopsLayer=L.geoJSON(storeList,{
+    onEachFeature:onEachFeature,
+    pointToLayer:function(feature,latlng){
+        return L.marker(latlng,{icon:myicon});
+    }
+
+});
+shopsLayer.addTo(map);
+function flyToStore(store){
+    const lat=store.geometry.coordinates[1];
+    const long=store.geometry.coordinates[0];
+map.flyTo([store.geometry.coordinates[1],store.geometry.coordinates[0]],14,{duration:3})
+setTimeout(()=>{
+    L.popup({closeButton:false,offset:L.point(0,-8)})
+    .setLatLng([lat,long])
+    .setContent(popupContent(store))
+    .openOn(map);
+},3000)
+ 
+}
---
+++ strore.js
@@ -0,0 +1,406 @@
+const storeList=[{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [80.24611199465274,26.511407391166596]
+    },
+    "properties": {
+      "name": "Kerla Cafe",
+      "address":"93-H, IIT Gate, Kalyanpur, Kanpur, Uttar Pradesh 208016",
+      "phone": "+919795941444"
+
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [80.34754518134436,26.474020770328]
+    },
+    "properties": {
+      "name": "Landmark Hotel",
+      "address":"Mall Rd, Civil Lines, Kanpur, Uttar Pradesh 208001",
+      "phone": "3344455677",
+
+    }
+  },{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [77.21624780043146,28.527066013072826]
+    },
+    "properties": {
+      "name": "Dakshin Restaurant New Delhi",
+      "address":"Saket District Centre, District Centre, Sector 6, Pushp Vihar, New Delhi, Delhi 110017",
+      "phone": "+911142661122",
+    }
+  },{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [77.23998206179907,28.596968905057214]
+    },
+    "properties": {
+      "name": "The Oberoi, New Delhi",
+      "address":"Dr Zakir Hussain Marg, Delhi Golf Club, Golf Links, New Delhi, Delhi 110003",
+      "phone": "+911124363030"
+    }
+  },{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [81.0082447965132,26.86442438334197]
+    },
+    "properties": {
+      "name": "Hilton Garden Inn Lucknow",
+      "address":"TCG -7/7, Gomti Nagar Rd, Vijaipur Colony, Vibhuti Khand, Gomti Nagar, Lucknow, Uttar Pradesh 226010",
+      "phone":"+915224277777"
+    }
+  },{
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [81.00958328487022,26.867960767231192]
+    },
+    "properties": {
+      "name": "Fairfield by Marriott Lucknow",
+      "address":"Gate No 2, opposite Indira Gandhi Pratishthan, Vijaipur Colony, Vibhuti Khand, Gomti Nagar, Lucknow, Uttar Pradesh 226010",
+       "phone":"+915226659999"
+    }
+  }, {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.85118403863889,19.07787862224536]
+    },
+    "properties": {
+      "name": "Grand Hyatt Mumbai Hotel & Residences",
+      "address": "Grand Hyatt Mumbai Hotel and Residences Bandra Kurla Complex Vicinity, Off, Western Express Hwy, Santacruz East, Mumbai, Maharashtra 400055",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [77.23824385423826,28.592003150705683]
+    },
+    "properties": {
+      "name": "Indian Accent",
+      "address": "The Lodhi, Lodhi Rd, CGO Complex, Pragati Vihar, New Delhi, Delhi 110003",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.82468278992283, 18.99953227821179]
+    },
+    "properties": {
+      "name": "Phoenix Palladium",
+      "address": "Unit NO. 6D, Block-17, Phoenix Mills Compound, 462, Senapati Bapat Marg, Lower Parel, Mumbai, Maharashtra 400013, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [80.28072648115932,26.49640683308553]
+    },
+    "properties": {
+      "name": "Lucky Restaurant Kanpur",
+      "address": "Shop No. 719, Grand Trunk Road Vikas Nagar, Lakhanpur, Kanpur, Uttar Pradesh 208024",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.83413472514303,18.9398169028641]
+    },
+    "properties": {
+      "name": "Cannon Pav Bhaji",
+      "address": " Mahapalika Marg, Opposite Chhatrapati Shivaji Terminus, Hazarimal Somani Road, Fort, Mumbai, Maharashtra 400001",
+      "phone": " 022 2207 4205"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [77.20889596191188, 28.68797557618975]
+    },
+    "properties": {
+      "name": "Domino's Pizza",
+      "address": "Ground Floor,Shop No. A,Municipal No. 26A & 27A-UA Jawahar Nagar,Kamla Nagar, near Malka Ganj, Chowk, Delhi 110007, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [77.11585549399085, 28.741270652258336]
+    },
+    "properties": {
+      "name": "Domino's Pizza Delhi",
+      "address": "SHOP NO. 10-11, COMMUNITY CENTRE, Garg Mall, Pocket 4, Sector 11, Rohini, New Delhi, Delhi 110020, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [80.24997475602336,26.495518389247547]
+    },
+    "properties": {
+      "name": "Hichki Family Restaurant",
+      "address": "Keshavpuram, Kalyanpur, Kanpur, Uttar Pradesh 208017",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [78.31630832696055,17.38877641178996]
+    },
+    "properties": {
+      "name": "Jewel of Nizam - The Minar",
+      "address": "The Golkonda Resorts & Spa The Golkonda Resorts & Spa, Sagarmahal Complex, Hyderabad 500075 India",
+      "phone": "+91 40 3069 6969"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [88.37799509128703, 22.632535906298756]
+    },
+    "properties": {
+      "name": "Khan pizza",
+      "address": "Ground Floor, Shop no. 3 & 4, Dist, Chinar Park, Noapara, Sukanta Pally, Newtown, Kolkata, West Bengal 700157, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [88.35190256153795, 22.56692525562484]
+    },
+    "properties": {
+      "name": "Food Outlet 2",
+      "address": "3A, Humayun Place Near New Empire Cinema, Esplanade, New Market Area, Dharmatala, Kolkata, West Bengal 700087, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [88.38795145132286, 22.70413303586267]
+    },
+    "properties": {
+      "name": "Pizza Shop3",
+      "address": "Municipality, 352/1889, Dakshminayan Barasat Road Under Panhihati, Sodepur, Kolkata, West Bengal 700110, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [88.3251233867392, 22.520947492726815]
+    },
+    "properties": {
+      "name": "Food Outlet 4",
+      "address": "Plot No. 99A, Block F, Nalini Ranjan Ave, New Alipore, Kolkata, West Bengal 700053, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.52382422967368, 23.080883179752515]
+    },
+    "properties": {
+      "name": "Food Outlet 5",
+      "address": "Plot No. 50 & 51, Survey No. 301/2, 313/1, 304/2, Shop No. G - 7 & 8 Ground Floor, Opp. Gujarat High Court, S. G. Road Town Planning Scheme No. 28, Ahmedabad, Gujarat 380061, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.58939887680621, 23.055613560773505]
+    },
+    "properties": {
+      "name": "Food Outlet 6",
+      "address": "Neelgagan Plaza, 1st And 2nd Floor Survey Number 2259 Commissioner Office, Dr Ambedkar Rd, Opposite Police, Jain Colony, Shahibag, Ahmedabad, Gujarat 380004, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.47507239777408, 23.033498752157918]
+    },
+    "properties": {
+      "name": "Food Outlet 7",
+      "address": "A1 Amrapali Axiom, Sardar Patel Ring Rd, Ambli, Ahmedabad, Gujarat 380058, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [72.66939307985272, 23.05340224332604]
+    },
+    "properties": {
+      "name": "Food Outlet 8",
+      "address": "Shop Number A/03 & A/04, Ground Floor, Gujarat 382350, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [73.87720724616587, 18.57728849915506]
+    },
+    "properties": {
+      "name": "Food Outlet 9",
+      "address": "Shop no 31/A, Ground floor, Shoppers Orbit, Next Mahalaxsmi Complex Near Visharantwadi Police Chowki, Dhanori Rd, Pune, Maharashtra 411015, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [73.93213888774288, 18.495259471467133]
+    },
+    "properties": {
+      "name": "Food Outlet 10",
+      "address": "Shop No:4,5,14,15,16 & 17 Fortune Plaza, Hadapsar, Pune, Maharashtra 411028, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [73.82364889562831, 18.48256098719139]
+    },
+    "properties": {
+      "name": "Food Outlet 11",
+      "address": "Plot No. 7, Dhanlaxmi Society, Opp. Jagtap Hospital, Pune, Maharashtra 411051, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [73.84321829294011, 18.52228131664155]
+    },
+    "properties": {
+      "name": "Food Outlet 12",
+      "address": "Bus Stop, Shop No. 6 & 7, opposite Deccan Gymkhana, Pune, Maharashtra 411004, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [73.79308003312877, 18.496413470622766]
+    },
+    "properties": {
+      "name": "Food Outlet 13",
+      "address": "Cipla Foundation, Survey No. 116/1, Shop No. 2, Opposite, Warje, Pune, Maharashtra 411058, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [78.68705181099095, 10.799619385080312]
+    },
+    "properties": {
+      "name": "Food Outlet 14",
+      "address": "Dukes Complex, 120, Bharathiar Salai, Melapudur, Cantonment, Tiruchirappalli, Tamil Nadu 620001, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [78.68405585878058, 10.834835813062087]
+    },
+    "properties": {
+      "name": "Food Outlet 15",
+      "address": "Vignesh Plaza, A-6, Ground Floor, Thillai Nagar Main Rd, Tiruchirappalli, Tamil Nadu 620018, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [74.63322891528135, 26.582440343492785]
+    },
+    "properties": {
+      "name": "Food Outlet 16",
+      "address": "Shop No. G5, G6 and G7, Ground Floor, Jaipur Rd, Hathi Bhata, Ajmer, Rajasthan 305001, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [75.12212052531665, 27.779609685150188]
+    },
+    "properties": {
+      "name": "Food Outlet 17",
+      "address": "Domino'S Pizza Ground Floor Shop No 2 Shiv Mandir cinema, Fatehpur Rd, Sikar, Rajasthan 332001, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [76.77930464002718, 28.378539373887044]
+    },
+    "properties": {
+      "name": "Food Outlet 18",
+      "address": "Khasra No. 489 & 490, V Square Mall, Alwar Bypass Road, Sector-15, Bhiwadi, Rajasthan 301019, India",
+      "phone": "23 2323 2323"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [75.85537784187994, 25.13798803924888]
+    },
+    "properties": {
+      "name": "Food Outlet 19",
+      "address": "A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+      "phone": "23 2323 2323"
+    }
+  }
+]
